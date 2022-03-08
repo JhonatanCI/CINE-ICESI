9df6b5f5e1486a0d8eca1760c2683482a17a2b89
Jhonatan Castaño <jhonatancastanoibarra@gmail.com>
2022-03-07 19:53:13 -0500

    using warnings and develop of register function

--- src/control/ControllerRegisterFunction.java
+++ src/control/ControllerRegisterFunction.java
@@ -3,6 +3,7 @@ package control;
 import exceptions.MissingInformation;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import model.Function;
@@ -44,7 +45,7 @@ public class ControllerRegisterFunction {
     	try{
     		TextField[] nulls = {nameTF,dayTF,hourHourTF,durationHourTF,roomFT,monthTF,hourMinutesTF,durationMinutesTF};
     		for (int i=0;i<nulls.length;i++) {
-				if(nulls[i]==null) {
+				if(nulls[i].getText().equals("")) {
 					throw new MissingInformation();
 				}
 			}
@@ -52,9 +53,15 @@ public class ControllerRegisterFunction {
     		int hourMinutes = Integer.parseInt(hourMinutesTF.getText());
     		int durationHour = Integer.parseInt(durationHourTF.getText());
     		int durationMinutes = Integer.parseInt(durationMinutesTF.getText());
-    		FunctionsRegister.addfunction(new Function(nameTF.getText(),roomFT.getText(),hourStart,hourMinutes,durationHour,durationMinutes,dayTF.getText(),monthTF.getText()));
-    	}catch(MissingInformation ex) {
     		
+    		FunctionsRegister.addfunction(new Function(nameTF.getText(),roomFT.getText(),hourStart,hourMinutes,durationHour,durationMinutes,dayTF.getText(),monthTF.getText()));
+    		
+    	}catch(MissingInformation ex) {
+    		Alert alert = new Alert(Alert.AlertType.WARNING);
+		    alert.setHeaderText(null);
+		    alert.setTitle("Some camps need to be especify");
+		    alert.setContentText("Please write all the spaces");
+		    alert.showAndWait();
     	}
     }
 
--- src/model/FunctionsRegister.java
+++ src/model/FunctionsRegister.java
@@ -2,13 +2,19 @@ package model;
 
 import java.util.ArrayList;
 
+import javafx.scene.control.Alert;
+
 public class FunctionsRegister {
 	private static ArrayList<Function> functions = new ArrayList<Function>();
 	
 	public static void addfunction(Function fun) {
 		for ( Function i : functions) {
 			if(i.getName().equalsIgnoreCase(fun.getName())) {
-				
+				Alert alert = new Alert(Alert.AlertType.ERROR);
+			    alert.setHeaderText(null);
+			    alert.setTitle("Error the function already exist");
+			    alert.setContentText("The name is already using please write other name");
+			    alert.showAndWait();
 			}
 			
 		}
